d5329168d25ec92951276f0780189a0394368cc1
한동곤[DongGon Han] <hdg@sorin.co.kr>
2022-06-15 09:04:51 +0900

    프로그래머스 1단계: 신고 결과 받기

---
+++ programmers/1level/신고 결과 받기/code.js
@@ -0,0 +1,22 @@
+function solution(id_list, reports, k) {
+    let idOfReport = {};
+    for(let id of id_list) {
+        idOfReport[id] = new Set();
+        idOfReport[id].count = 0;
+    }
+    
+    for(let report of reports) {
+        let [report_id, use_id] = report.split(" ");
+        idOfReport[use_id].add(report_id);
+    }
+    
+    for(let id of id_list) {
+        if(idOfReport[id].size >= k) {
+            for(let report_id of Array.from(idOfReport[id])) {
+                idOfReport[report_id].count++;
+            }
+        }
+    }
+    return Array(id_list.length).fill()
+                                .map((_,index)=>idOfReport[id_list[index]].count);
+}
