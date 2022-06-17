6638e8072e3d0ff8be333c45c0ef6b1bbd415b15
jayo9901 <100823870+jayo9901@users.noreply.github.com>
2022-06-17 19:54:39 +0900

    기본 틀

--- programmers/1level/키패드 누르기_재윤/code.py
+++ programmers/1level/키패드 누르기_재윤/code.py
@@ -1,3 +1,18 @@
 def solution(numbers, hand):
     answer = ''
     return answer
+
+left 1 4 7
+1 1 2 3 4
+4 2 1 2 3
+7 3 2 1 2
+
+right 3 6 9
+3 1 2 3 4
+6 2 1 2 3
+9 3 2 1 2
+
+2 5 8 0
+
+if le<ra = le
+elit le>ra = ra
