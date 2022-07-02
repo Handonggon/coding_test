24e432559d686a1bca1907f97d56f781966e10b7
Handonggon <52454879+Handonggon@users.noreply.github.com>
2022-07-02 13:00:37 +0900

    Update code.py

--- programmers/1level/숫자 문자열과 영단어/code.py
+++ programmers/1level/숫자 문자열과 영단어/code.py
@@ -1,3 +1,40 @@
+from enum import Enum
+class NUMBER(Enum):
+    ZERO  = "0"
+    ONE   = "1"
+    TWO   = "2"
+    THREE = "3" 
+    FOUR  = "4"
+    FIVE  = "5"
+    SIX   = "6"
+    SEVEN = "7"
+    EIGHT = "8"
+    NINE  = "9"
+    
+class Enum:
+    @staticmethod
+    def create(*sequential, **named):
+        enums = dict(zip(sequential, range(len(sequential))), **named)
+        for key, value in enums.items():
+            infoFn = lambda s: str(value)
+            enums[key] = type(key, (), {"__str__": infoFn, "__repr__": infoFn, "name": key})()
+        enums["keys"] = list(enums.keys())
+        enums["index"] = 0
+        enums["__iter__"] = lambda self: self
+        def __next__(self):
+            if self.index >= len(self.keys):
+                self.index = 0
+                raise StopIteration
+            enum = enums[self.keys[self.index]]
+            value = type("t", (), {"name": enum.name, "value": enum})()
+            self.index += 1
+            return value
+        enums["__next__"] = __next__
+        return type('Enum', (), enums)()
+
+
 def solution(s):
-    answer = ''
-    return answer
+    #NUMBER = Enum.create("ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE")
+    for number in Color:
+        s = s.replace(number.name.lower(), str(number.value))
+    return int(s)
