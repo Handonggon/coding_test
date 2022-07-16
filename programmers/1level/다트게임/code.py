c9c033d817db19d511f65e08a11ca84dc0446466
Handonggon <52454879+Handonggon@users.noreply.github.com>
2022-07-16 12:22:25 +0900

    Update code.py

--- programmers/1level/다트게임/code.py
+++ programmers/1level/다트게임/code.py
@@ -1,3 +1,30 @@
+import math
+
 def solution(dartResult):
-    answer = 0
-    return answer
+    stack = []
+    
+    index = 0
+    while index < len(dartResult):
+        if dartResult[index].isnumeric():
+            number = int(dartResult[index])
+            if number == 1 and dartResult[index+1] == '0':
+                stack.append(10)
+                index += 1
+            else:
+                stack.append(number)
+        else:
+            if dartResult[index] == 'S':
+                stack.append(pow(stack.pop(), 1))
+            elif dartResult[index] == 'D':
+                stack.append(pow(stack.pop(), 2))
+            elif dartResult[index] == 'T':
+                stack.append(pow(stack.pop(), 3))
+            elif dartResult[index] == '*':
+                curr = stack.pop()
+                if len(stack):
+                    stack.append(stack.pop() * 2)
+                stack.append(curr * 2)
+            elif dartResult[index] == '#':
+                stack.append(stack.pop() * -1)
+        index += 1
+    return sum(stack)
