fb0d5852faa2974ef2749f21b5160bbdfdf4f2f3
jayo9901 <100823870+jayo9901@users.noreply.github.com>
2022-07-17 10:22:25 +0900

    콜라츠 추측
    포문의 리스트를 읽는 방식을 이용한 루프

---
+++ programmers/1level/14.콜라츠 추측/code.py
@@ -0,0 +1,16 @@
+def solution(num):
+    list_n = []
+    list_n.append(num)
+    for i in list_n:
+        if len(list_n) == 501:
+            return -1
+            break
+        else:
+            if i == 1:
+                break
+            else:
+                if i % 2 == 0:
+                    list_n.append(int(i/2))
+                else:
+                    list_n.append(i*3+1)
+    return len(list_n)-1
