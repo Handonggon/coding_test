17fb053858623894c629c69106bf0185c3f81dde
Handonggon <52454879+Handonggon@users.noreply.github.com>
2022-08-17 16:48:40 +0900

    Update code.js

--- programmers/코딩테스트 실전 대비 모의고사(2022)/1차(7.13~8.23)/동곤/4번/code.js
+++ programmers/코딩테스트 실전 대비 모의고사(2022)/1차(7.13~8.23)/동곤/4번/code.js
@@ -3,12 +3,12 @@ function solution(beginning, target) {
     let M = beginning[0].length;
 
     let answer = N + M + 1;
-    for(let i = 0; i < Math.pow(2, N); i++) {
-        for(let j = 0; j < Math.pow(2, M); j++) {
+    for(let i = 0; i < (1 << N); i++) {
+        for(let j = 0; j < (1 << M); j++) {
             let check = true;
             for(let r = 0; r < beginning.length; r++) {
                 for(let c = 0; c < beginning[r].length; c++) {
-                    if(((Boolean(1 << r & i) ^ Boolean(1 << c & j)) ^ beginning[r][c]) === target[r][c]) continue;
+                    if(((Boolean((1 << r) & i) ^ Boolean((1 << c) & j)) ^ beginning[r][c]) === target[r][c]) continue;
                     check = false;
                     break;
                 }
