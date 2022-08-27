3f14e9281033722e92ff9ddac554588277dddd06
jayo9901 <100823870+jayo9901@users.noreply.github.com>
2022-08-28 00:36:30 +0900

    해결

--- programmers/2level/16.다리를 지나는 트럭/code.py
+++ programmers/2level/16.다리를 지나는 트럭/code.py
@@ -1,27 +1,29 @@
 from collections import deque
-def solution(bridge_length, weight, tw):
-    answer = 0
-    count = -1
-    bri = deque([])
-    brw = 0
-    des = []
-    
-    finish = tw
-    tw.reverse()
-    while des!= finish:
+def solution(length, weight, truck):
+    arrive = []
+    des = truck
+    bridge = deque()
+    truck = deque(truck)
+    count = 0
+    now = 0
+    while arrive != des:
         count += 1
-        if len(bri) > 0:
-            if count - bri[0][1] == bridge_length:
-                a = bri.popleft()
-                brw -= a[1]
-                des.append(a)                
-        b = tw.pop()
-        if brw + b <= weight:
-            bri.append([b,count])
-            brw += b
+        if len(bridge) > 0 :
+            a = bridge.popleft()
+            if count - a[1] == length:
+                arrive.append(a[0])
+                now -= a[0]
+            else:
+                bridge.appendleft(a)
         else:
-            tw.append(b)
-        
-        
-    
+            pass
+        if len(truck) > 0 :
+            b = truck.popleft()
+            if b + now > weight:
+                truck.appendleft(b)
+            else:
+                bridge.append([b,count])
+                now += b
+        else:
+            pass
     return count
