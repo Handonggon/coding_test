e869a193ad59e2026d9d84edf72daa60ce5bf175
Handonggon <gksehdrhs2@gmail.com>
2022-09-05 01:19:40 +0900

    파일정리

--- programmers/코딩테스트 실전 대비 모의고사(2022)/3차(8.10~8.23)/잉규/1번.py
+++
@@ -1,7 +0,0 @@
-def solution(a, b, n):
-    result = 0
-    while n >= a :
-        result += (n//a) * b
-        n = (n//a) * b + n%a 
-
-    return result
--- programmers/코딩테스트 실전 대비 모의고사(2022)/3차(8.10~8.23)/잉규/3번.py
+++
@@ -1,7 +0,0 @@
-def solution(distance, scope, times):
-    result_list = [distance]
-    for point in range(len(scope)) :
-        for i in range(min(scope[point])-1,max(scope[point])):
-            if i  % (times[point][0]+times[point][1]) < times[point][0] :
-                result_list.append(i+1)
-    return min(result_list)
