53218f0f8d3b618756cba7d03ab71e6721ad6e08
Handonggon <gksehdrhs2@gmail.com>
2022-09-14 21:06:45 +0900

    두수의 합

--- 백준/9024_두수의 합/code.py
+++ 백준/9024_두수의 합/code.py
@@ -1,7 +1,9 @@
+#이분 탐색 + 투포인터 풀이
 T = int(input())
 for _ in range(T):
     N, K = map(int, input().split())
     S = sorted(list(map(int, input().split())))
+
     term = 2 * pow(10, 8)
     for i in range(len(S)-2):
         left, right = i+1, len(S)-1
@@ -18,4 +20,31 @@ for _ in range(T):
             elif abs(S[i] + S[mid] - K) < term:
                 answer = 1
                 term = abs(S[i] + S[mid] - K)
+    print(answer)
+
+#투포인터 풀이
+T = int(input())
+for _ in range(T):
+    N, K = map(int, input().split())
+    S = sorted(list(map(int, input().split())))
+    term = 2 * pow(10, 8)
+    
+    left, right = 0, len(S)-1
+    while left < right:
+        sum = S[left] + S[right]
+
+        if abs(sum - K) == term:
+            answer += 1
+        elif abs(sum - K) < term:
+            answer = 1
+            term = abs(sum - K)
+        
+        if sum == K:
+            left += 1
+            right -= 1
+        elif sum < K:
+            left += 1
+        elif sum > K:
+            right -= 1
+
     print(answer)
